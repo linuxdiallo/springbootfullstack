ce0d5e34099afb81cd6305445870850792cae3a8
aboubacardiallo <aboubacardiallo594@outlook.fr>
2023-06-08 17:02:30 +0000

    test controller

---
+++ backend/src/test/java/com/codepro/customer/TestController.java
@@ -0,0 +1,11 @@
+package com.codepro.customer;
+
+/**
+ * @author aboubacar.diallo
+ * @created 08/06/2023 - 17:01
+ * @project springbootfullstack
+ * @package com.codepro.customer
+ */
+
+public class TestController {
+}
