5090612e20bbf377d5451dea6c4dc5c368aab7b4
Aboubacar_DIALLO <aboubacard659@gmail.com>
2024-01-22 12:35:59 +0000

    add comment for single pattern

--- backend/src/main/java/com/codepro/RandomSingleInstance.java
+++ backend/src/main/java/com/codepro/RandomSingleInstance.java
@@ -2,6 +2,9 @@ package com.codepro;
 
 import java.util.Random;
 
+/***
+ * This use singleton pattern
+ */
 public class RandomSingleInstance {
     private static Random INSTANCE;
     public static Random getInstance() {
