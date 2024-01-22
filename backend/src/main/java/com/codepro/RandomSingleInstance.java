d6390a95e7128a2d1462933382d0f6b10d891b2e
Aboubacar_DIALLO <aboubacard659@gmail.com>
2024-01-22 12:28:10 +0000

    update RandomSingelInstance class

--- backend/src/main/java/com/codepro/RandomSingleInstance.java
+++ backend/src/main/java/com/codepro/RandomSingleInstance.java
@@ -4,7 +4,6 @@ import java.util.Random;
 
 public class RandomSingleInstance {
     private static Random INSTANCE;
-
     public static Random getInstance() {
         if (INSTANCE == null) {
             INSTANCE = new Random();
